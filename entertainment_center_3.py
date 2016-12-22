e13e087f5ac6fee6d50d9b673ecdce040fd9d0b0
Robert Navarro <rnavarro@stitchlabs.com>
2016-12-21 19:23:31 -0800

    Fixed URL splitting as per @jusung806's comments. URLs aren't supposed to be split on multiple lines

--- entertainment_center_3.py
+++ entertainment_center_3.py
@@ -14,9 +14,7 @@ fast_and_furious = media.Movie("The Fast and the Furious",
 minions = media.Movie("Minions",
                       "A movie about evil helpers trying to take over the "
                       "world",
-                      "https://upload.wikimedia.org/wikipedia/en/3/3d"
-                      "/Minions_poster.jpg",
-                      # NOQA
+                      "https://upload.wikimedia.org/wikipedia/en/3/3d/Minions_poster.jpg",  # NOQA
                       "https://www.youtube.com/watch?v=Wfql_DoHRKc")  # NOQA
 # print(minions.storyline)
 # minions.show_trailer()
@@ -24,9 +22,7 @@ minions = media.Movie("Minions",
 mulan = media.Movie("Mulan",
                     "A movie about a girl trying to save her father and "
                     "China.",
-                    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a3"
-                    "/Movie_poster_mulan.JPG/220px-Movie_poster_mulan.JPG",
-                    # NOQA
+                    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/Movie_poster_mulan.JPG/220px-Movie_poster_mulan.JPG",  # NOQA
                     "https://www.youtube.com/watch?v=wAbGAkkOgcM")  # NOQA
 # print(mulan.storyline)
 # mulan.show_trailer()
@@ -34,19 +30,14 @@ mulan = media.Movie("Mulan",
 lilo_and_stitch = media.Movie("Lilo and Stitch",
                               "A movie about aliens creations and the meaning "
                               "of family in Hawaii",
-                              "http://www.gstatic.com/tv/thumb/movieposters"
-                              "/29095/p29095_p_v8_aa.jpg",
-                              # NOQA
+                              "http://www.gstatic.com/tv/thumb/movieposters /29095/p29095_p_v8_aa.jpg",  # NOQA
                               "https://www.youtube.com/watch?v=hu9bERy7XGY")
 # NOQA
 # print(lilo_and_stitch.storyline)
 # lilo_and_stitch.show_trailer()
 nerve = media.Movie("Nerve",
                     "A movie about a deathly game of truth and dare",
-                    "https://images-na.ssl-images-amazon.com/images/M"
-                    "/MV5BMTUzOTg1OTM4NV5BMl5BanBnXkFtZTgwMTg2Mjg0OTE"
-                    "@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                    # NOQA
+                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzOTg1OTM4NV5BMl5BanBnXkFtZTgwMTg2Mjg0OTE@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=Tyk2_bzca3E")  # NOQA
 # print(nerve.storyline)
 # nerve.show_trailer()
@@ -54,10 +45,7 @@ nerve = media.Movie("Nerve",
 a_walk_to_remember = media.Movie("A Walk to Remember",
                                  "A lovestory about a dying girl befriending "
                                  "a bully",
-                                 "https://upload.wikimedia.org/wikipedia/en/"
-                                 "thumb/d/dc/A_Walk_to_Remember_Poster.jpg/"
-                                 "220px-A_Walk_to_Remember_Poster.jpg",
-                                 # NOQA
+                                 "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/A_Walk_to_Remember_Poster.jpg/220px-A_Walk_to_Remember_Poster.jpg",  # NOQA
                                  "https://www.youtube.com/watch?v=EgdoQ8Oxu2E")  # NOQA
 # print(a_walk_to_remember)
 # a_walk_to_remember.show_trailer()
